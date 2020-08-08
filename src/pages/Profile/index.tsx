1b33677a1769532c329719e66de59435940bb604
Caroline Ribeiro <caroline.ribeiro@guiabolso.com.br>
2020-08-07 21:09:37 -0300

    calendar with random contributions finished

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-import { Container, Main, LeftSide, RightSide, Repos } from "./styles";
+import { Container, Main, LeftSide, RightSide, Repos, CalendarHeading } from "./styles";
 
 import ProfileData from "../../components/ProfileData";
 import RepoCard from "../../components/RepoCard";
+import RandomCalendar from "../../components/RandomCalendar";
 
 const Profile: React.FC = () => {
 	return (
@@ -39,7 +40,13 @@ const Profile: React.FC = () => {
 								/>
 							})}
 						</div>
-					</Repos>					
+					</Repos>
+					
+					<CalendarHeading>
+						Random calendar (do not represent actual data)
+					</CalendarHeading>
+
+					<RandomCalendar />
 				</RightSide>
 			</Main>
 		</Container>
